58e7dee7c1bcc711c935079a7b581f0bd6637067
Brasil <matheus.brasil@accenture.com>
2021-02-19 16:51:45 +0000

    Added the setup for SAP Cloud and changed the mockdata.

--- services/sortingHat.service.js
+++ services/sortingHat.service.js
@@ -1,5 +1,6 @@
-const express = require('express')
-var path = require('path')
+const express = require('express');
+const path = require('path');
+const hogwartsHouses = require('../public/mockdata/houses.json');
 
 class Houses{
     constructor(houseName, houseDescription, notableMembers,imageLink) {
@@ -19,51 +20,9 @@ function getRandomInt(min, max){
 module.exports = {
     sortingHat: (req, res) => {
         // Returns a random House
-        var hogwartsHouses = [
-            new Houses(
-                'Gryffindor',
-                'You might belong in Gryffindor, Where dwell the brave at heart, Their daring, nerve and chivalry Set Gryffindors apart.',
-                [
-                    'Harry Potter',
-                    'Hermione Granger',
-                    'Ron Weasley'
-                ],
-                new URL(req.protocol + "://" + req.get("host") + "/images/Gryffindor_Crest.jpg")
-                ),
-            new Houses(
-                'Hufflepuff',
-                'You might belong in Hufflepuff Where they are just and loyal Those patient Hufflepuffs are true And unafraid of toil.',
-                [
-                    'Newt Scamander',
-                    'Cedric Diggory',
-                    'Nymphadora Tonks'
-                ],
-                new URL(req.protocol + "://" + req.get("host") + "/images/Hufflepuff_Crest.jpg")
-            ),
-            new Houses(
-                'Ravenclaw',
-                'Or yet in wise old Ravenclaw If you’ve a ready mind Where those of wit and learning Will always find their kind.',
-                [
-                    'Luna Lovegood',
-                    'Gilderoy Lockhart',
-                    'Filius Flitwick'
-                ],
-                new URL(req.protocol + "://" + req.get("host") + "/images/Ravenclaw_Crest.jpg")
-            ),
-            new Houses(
-                'Slytherin',
-                'Or perhaps in Slytherin You’ll make your real friends Those cunning folk use any means To achieve their ends.',
-                [
-                    'Severus Snape',
-                    'Draco Malfoy',
-                    '(rather unfortunately) Lord Voldemort'
-                ],
-                new URL(req.protocol + "://" + req.get("host") + "/images/Slytherin_Crest.jpg")
-            )
-        ];
+        var hogwartsHouse = Object.assign({}, hogwartsHouses[getRandomInt(0,3)]);
+        hogwartsHouse.imageLink = new URL(req.protocol + "://" + req.get("host") + hogwartsHouse.imageLink);
 
-        //console.dir(req.protocol + '://' + req.get('host') + req.originalUrl);
-
-        return res.status(200).json({ sortingHat: hogwartsHouses[getRandomInt(0,3)] })
+        return res.status(200).json({ sortingHat: hogwartsHouse })
     }
 }
